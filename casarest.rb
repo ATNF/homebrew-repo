fe1fbdbb4d35390dd87bc536c8c22102926f5b91
Stephen Ord <steve.ord@gmail.com>
2023-06-21 16:40:35 +1000

    added the rest of the casa formula

---
+++ casarest.rb
@@ -0,0 +1,40 @@
+class Casarest < Formula
+  desc 'The light-weight imager (lwimager) for radio astronomy'
+  homepage 'https://github.com/ska-sa/meqtrees/wiki/LinkingWithCasaCore'
+  url 'https://svn.astron.nl/casarest/release/casarest/release-1.2.1'
+  head 'https://svn.astron.nl/casarest/trunk/casarest'
+
+  depends_on 'cmake' => :build
+  depends_on 'casacore'
+  depends_on 'boost'
+  depends_on 'readline'
+  depends_on 'wcslib'
+  depends_on 'hdf5'
+  depends_on 'gcc'
+
+  # def patches
+  #   p = []
+  #   # Fixes disallowed size_t vs int* comparison, which used to be specially
+  #   # included for Darwin systems, but does not seem relevant anymore (fixed in HEAD).
+  #   p << 'https://gist.github.com/raw/4705907/678753a3fc04751457271c82f4a3fe39149b5819/patch1.diff' if not build.head?
+  #   # Add boost_system library to avoid missing symbols (fixed in HEAD)
+  #   p << 'https://gist.github.com/raw/4705907/a199623d33e3dd566a8ffe15cc0448f6e771e44d/patch2.diff' if not build.head?
+  #   return p.empty? ? nil : p
+  # end
+
+  def install
+    # Workaround to get fortran and C++ to play together (see Homebrew issue #20173)
+    ENV.append 'LDFLAGS', "-L/usr/lib -lstdc++"
+    # Force clang to use the old standard library for now (solves issue with complex type)
+    ENV.append 'CXXFLAGS', "-stdlib=libstdc++" if ENV.compiler == :clang
+    mkdir_p 'build'
+    cd 'build'
+    cmake_args = std_cmake_args
+    cmake_args << "-DCASACORE_ROOT_DIR=#{HOMEBREW_PREFIX}"
+    cmake_args << "-DHDF5_ROOT_DIR=#{HOMEBREW_PREFIX}"
+    system 'cmake', '..', *cmake_args
+    system "make install"
+    mkdir_p "#{share}/casarest"
+    mv '../measures_data', "#{share}/casarest/data"
+  end
+end
---
+++ python-casacore@3.5.2.rb
@@ -0,0 +1,39 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class PythonCasacoreAT352 < Formula
+  include Language::Python::Virtualenv
+
+  desc ""
+  homepage ""
+  url "https://github.com/casacore/python-casacore/archive/refs/tags/v3.5.2.tar.gz"
+  version "3.5.2"
+  sha256 "eb65a6903ca956dc6dd0ad1d14f17ae622c7a6b83826c6aede42428ae7314c2e"
+  license ""
+
+  depends_on "python@3.11"
+  depends_on "casacore" => :build
+  depends_on "boost-python3" => :build 
+
+  def python3
+    "python3.11"
+  end
+
+  def install
+  	system python3, "setup.py", "build_ext", "-I/opt/homebrew/include",
+	                                           "-L/opt/homebrew/lib"
+    system python3, *Language::Python.setup_install_args(prefix, python3)
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test python-casacore@3.5.2`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+  end
+end
