9f397c9d24bd882759b308371a5944631962ecdc
Stephen Ord <steve.ord@gmail.com>
2023-09-01 15:01:04 +1000

    Added log4cxx

---
+++ log4cxx@0.11.rb
@@ -0,0 +1,56 @@
+class Log4cxxAT011 < Formula
+  desc "Library of C++ classes for flexible logging"
+  homepage "https://logging.apache.org/log4cxx/index.html"
+  url "https://archive.apache.org/dist/logging/log4cxx/0.11.0/apache-log4cxx-0.11.0.tar.gz"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "apr-util"
+
+  fails_with gcc: "5" # needs C++17 or Boost
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON"
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <log4cxx/logger.h>
+      #include <log4cxx/propertyconfigurator.h>
+      int main() {
+        log4cxx::PropertyConfigurator::configure("log4cxx.config");
+
+        log4cxx::LoggerPtr log = log4cxx::Logger::getLogger("Test");
+        log->setLevel(log4cxx::Level::getInfo());
+        LOG4CXX_ERROR(log, "Foo");
+
+        return 1;
+      }
+    EOS
+    (testpath/"log4cxx.config").write <<~EOS
+      log4j.rootLogger=debug, stdout, R
+
+      log4j.appender.stdout=org.apache.log4j.ConsoleAppender
+      log4j.appender.stdout.layout=org.apache.log4j.PatternLayout
+
+      # Pattern to output the caller's file name and line number.
+      log4j.appender.stdout.layout.ConversionPattern=%5p [%t] (%F:%L) - %m%n
+
+      log4j.appender.R=org.apache.log4j.RollingFileAppender
+      log4j.appender.R.File=example.log
+
+      log4j.appender.R.MaxFileSize=100KB
+      # Keep one backup file
+      log4j.appender.R.MaxBackupIndex=1
+
+      log4j.appender.R.layout=org.apache.log4j.PatternLayout
+      log4j.appender.R.layout.ConversionPattern=%p %t %c - %m%n
+    EOS
+    system ENV.cxx, "-std=c++17", "test.cpp", "-o", "test", "-L#{lib}", "-llog4cxx"
+    assert_match(/ERROR.*Foo/, shell_output("./test", 1))
+  end
+end
